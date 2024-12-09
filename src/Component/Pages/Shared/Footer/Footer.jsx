b631bfe6c143239397e4eca36303c517f41c0785
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-12-09 22:56:42 +0600

    banner navbar and footer added

---
+++ src/Component/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+    return (
+        <footer >
+            <div className=" bg-[#1F2937] text-neutral-content p-10 flex justify-center gap-80">
+              
+                    <div >
+                        <h2 className="text-3xl font-medium text-center">CONTACT US</h2>
+                        <div className="text-lg font-medium text-center">
+                            <h3>34/8, Talaimari, Rajshahi</h3>
+                            <p>+880 1777777777</p>
+                            <p>Mon - Fri : 08:00 - 22:00</p>
+                            <p>Sat - Sun : 10:00 - 23:00</p>
+                        </div>
+                    </div>
+               
+               
+                    <div className="text-center space-y-6">
+                        <h2 className="text-3xl font-medium">Follow Us</h2>
+                        <p>Join us on social media</p>
+                        <div className="flex text-xl gap-4 justify-center">
+                         
+                            <a href=""><FaFacebook></FaFacebook></a>
+                            <a href=""><FaInstagram></FaInstagram></a>
+                            <a href=""><FaTwitter></FaTwitter></a>
+
+
+                        </div>
+                    </div>
+                
+            </div>
+            <div className=" bg-black text-neutral-content p-4 text-center">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by Hungry Restaurant</p>
+                </aside>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Component/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link, NavLink } from "react-router-dom";
+import '../Navbar/Navbar.css'
+const Navbar = () => {
+    const navOptions =
+        <>
+           <NavLink to="/">HOME</NavLink>
+           <Link>CONTACT US</Link>
+           <Link>DASHBOARD</Link>
+           <Link>DASHBOARD</Link>
+           <Link>OUR SHOP</Link>
+        </>
+    return (
+        <div className="navbar fixed z-10 bg-opacity-30 bg-[#151515] md:flex md:justify-between max-w-screen-xl text-white">
+            <div >
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-lg font-extrabold">
+                        {navOptions}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case"><span className="text-3xl font-black">HUNGRY</span><span className="text-2xl font-bold">RESTAURANT</span></a>
+            </div>
+            <div >
+            <div className=" hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-lg font-extrabold gap-3">
+                    {navOptions}
+                </ul>
+            </div>
+            <div >
+                <a className="btn">Get started</a>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
