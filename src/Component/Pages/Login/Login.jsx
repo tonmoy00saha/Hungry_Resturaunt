c3bcd6ae0f9d80ee0fc95226da21864138fe10b3
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-12-19 21:01:42 +0600

    captcha added to the login page

--- src/Component/Pages/Login/Login.jsx
+++ src/Component/Pages/Login/Login.jsx
@@ -1,17 +1,40 @@
+import { IoMdCheckmark } from "react-icons/io";
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { Helmet } from "react-helmet";
 import authenticationimg from '../../../assets/others/authentication1.png';
 import background from '../../../assets/others/authentication.png'
+import { useEffect, useRef, useState } from 'react';
 const Login = () => {
     const myStyle = {
         backgroundImage: `url(${background})`,
     }
-    const handleLogin = e =>{
+    const captchaRef = useRef(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleLogin = e => {
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+
+
+    
     }
+    const handleCaptcha = e =>{
+        const user_captcha_value = captchaRef.current.value;
+        if(validateCaptcha(user_captcha_value)==true){
+            setDisabled(false);
+        }
+        else{
+            setDisabled(true);
+        }
+    }
+
+    useEffect(()=>{
+        loadCaptchaEnginge(6);
+    },[])
+
     return (
         <div>
             <Helmet>
@@ -20,7 +43,7 @@ const Login = () => {
             <div style={myStyle} className="hero min-h-screen bg-base-200 p-28">
                 <div className="hero-content flex flex-col md:flex-row md:gap-48">
                     <div className="">
-                       <img src={authenticationimg} alt="" />
+                        <img src={authenticationimg} alt="" />
                     </div>
                     <form onSubmit={handleLogin} className="card flex-shrink-0 w-full max-w-sm " id="">
                         <h2 className="text-3xl font-semibold text-center">Login</h2>
@@ -40,8 +63,17 @@ const Login = () => {
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <LoadCanvasTemplate />
+                                </label>
+                                <div className="flex">
+                                <input ref={captchaRef}  type="text" name="captcha" placeholder="Type the captcha above" className="input input-bordered w-full" />
+                                <button className="btn p-4 border-0" onClick={handleCaptcha}><IoMdCheckmark></IoMdCheckmark></button>
+                                </div>
+                            </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button disabled={disabled} className="btn bg-[#D1A054B3] text-white">Login</button>
                             </div>
                         </div>
                     </form>
