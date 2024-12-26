8f5b9b31423456752e799528619f8c219cc83026
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-12-27 05:34:11 +0600

    rechart added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -43,14 +43,16 @@ const AuthProvider = ({ children }) => {
                 .then(res=>{
                     if(res.data.token){
                         localStorage.setItem('access-token', res.data.token);
+                        setLoading(false);
                     }
                 })
             }
             else
             {
                 localStorage.removeItem('access-token');
+                setLoading(false);
             }
-            setLoading(false);
+            
         });
         return ()=>{
             return unSubscribe();
